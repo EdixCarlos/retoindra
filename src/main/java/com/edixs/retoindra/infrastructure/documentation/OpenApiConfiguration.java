a7768fdebea8fd47755bb8dd6fa58283feb6ebb7
Edix Carlos <edix.carlos@tecsup.edu.pe>
2025-02-19 23:46:00 -0500

    contact

--- src/main/java/com/edixs/retoindra/infrastructure/documentation/OpenApiConfiguration.java
+++ src/main/java/com/edixs/retoindra/infrastructure/documentation/OpenApiConfiguration.java
@@ -1,6 +1,7 @@
 package com.edixs.retoindra.infrastructure.documentation;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +14,13 @@ public class OpenApiConfiguration {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("PowerUp API")
-                        .version("1.0")
+                        .title("Reto Indra API")
+                        .version("0.0.1-SNAPSHOT")
                         .description("API de ejemplo utilizando Clean Architecture")
+                        .contact(new Contact()
+                                .name("Edix Carlos")
+                                .url("https://edixcarlos.tech/")
+                                .email("edix.carlos@tecsup.edu.pe"))
                         .license(new License().name("MIT License").url("https://opensource.org/licenses/MIT")));
     }
 }
